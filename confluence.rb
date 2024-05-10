6442c5ab32d39d979e9350d83c7e36b74b73150d
nycnog <nyc.gui@gmail.com>
2024-05-09 22:20:07 -0300

    ajustes no modulo e movendo acoes do jira para o modulo jira

--- confluence.rb
+++ confluence.rb
@@ -1,8 +1,3 @@
-require 'httparty'
-require 'pry'
-require 'nokogiri'
-require 'json'
-
 module Confluence
 
   def consulta_base_cts(idPagina, token)
@@ -38,92 +33,20 @@ module Confluence
     tabela
   end
 
-  def cria_card_jira(token)
-    url = "https://nycnog.atlassian.net/rest/api/3/issue?updateHistory=true&applyDefaultValues=false&skipAutoWatch=true"
-    headers = {
-      'Accept' => 'application/json',
-      'content-type' => 'application/json',
-      'Authorization' => "Basic #{token}"
-    }
-
-    body = {
-      "fields": {
-        "project": {
-          "id": "10000"
-        },
-        "issuetype": {
-          "id": "10005"
-        },
-        "summary": "Regressivo",
-        "reporter": {
-          "id": "5e46ef46ab90210c8de0ee8e"
-        }
-      }
-    }
-
-    retorno = HTTParty.post(url, headers: headers, body: body.to_json)
-  end
-
-  def cria_card_jira_filho(token, idPai, idCt, nomeCt, gherking)
-    url = "https://nycnog.atlassian.net/rest/api/3/issue?updateHistory=true&applyDefaultValues=false&skipAutoWatch=true"
-    headers = {
-      'Accept' => 'application/json',
-      'content-type' => 'application/json',
-      'Authorization' => "Basic #{token}"
-    }
-
-    body = {
-      "fields": {
-        "project": {
-          "id": "10000"
-        },
-        "issuetype": {
-          "id": "10003"
-        },
-        "summary": "#{idCt} | #{nomeCt}",
-        "parent": {
-          "id": idPai
-        },
-        "description": {
-          "version": 1,
-          "type": "doc",
-          "content": [
-            {
-              "type": "paragraph",
-              "content": [
-                {
-                  "type": "text",
-                  "text": gherking
-                }
-              ]
-            }
-          ]
-        },
-        "labels": [],
-        "reporter": {
-          "id": "5e46ef46ab90210c8de0ee8e"
-        }
-      }
-    }
-
-    retorno = HTTParty.post(url, headers: headers, body: body.to_json)
-  end
-
   module_function :consulta_base_cts
   module_function :filtra_tabela
-  module_function :cria_card_jira
-  module_function :cria_card_jira_filho
+
 end
 
 
-idPagina = ARGV[0]
-token = ARGV[1]
+# idPagina = ARGV[0]
+# token = ARGV[1]
 
-retorno = Confluence.consulta_base_cts(idPagina, token)
-tabela = Confluence.filtra_tabela(retorno)
+# retorno = Confluence.consulta_base_cts(idPagina, token)
+# tabela = Confluence.filtra_tabela(retorno)
 
-retornoJira = Confluence.cria_card_jira(token)
-idPai = retornoJira["id"]
-tabela.each do |cenario|
-  retornoJira = Confluence.cria_card_jira_filho(token, idPai, cenario["Id ct"], cenario["ct"], cenario["gherking"])
-end
+# retornoJira = Confluence.cria_card_jira(token)
+# idPai = retornoJira["id"]
+# tabela.each do |cenario|
+#   retornoJira = Confluence.cria_card_jira_filho(token, idPai, cenario["Id ct"], cenario["ct"], cenario["gherking"])
+# end
